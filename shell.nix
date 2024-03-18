e193ec2a9d4c22b8b6097ca0a41a428992999f81
vlad <vlad.anghel@protonmail.com>
2024-03-18 12:42:07 +0200

    nix: add Catppuccin theme package

--- shell.nix
+++ shell.nix
@@ -14,6 +14,27 @@ let
 	'';
 
 
+	ctpTW = pkgs.buildNpmPackage rec {
+		pname = "catppuccin-tailwindcss";
+		version = "0.1.6";
+		src = pkgs.fetchFromGitHub {
+			owner = "catppuccin";
+			repo = "tailwindcss";
+			rev = "v${version}";
+			hash = "sha256-ae5v9cB21Rs6O1+Y9QgoNr2e3Qio5MEXuh84dkgcg0A=";
+		};
+		npmDepsHash = "sha256-Q4b0WLUBLPfArieC7D0h/KfX/wpB/MgZIk0VzYIO1IQ=";
+		npmPackFlags = [ "--ignore-scripts" ];
+		NODE_OPTIONS = "--openssl-legacy-provider";
+
+		meta = with pkgs.lib; {
+			description = "A modern web UI for various torrent clients with a Node.js backend and React frontend";
+			homepage = "https://flood.js.org";
+			license = licenses.gpl3Only;
+			maintainers = with maintainers; [ winter ];
+		};	
+	};
+
 in
 pkgs.mkShell {
 	# Fix for delve
@@ -26,6 +47,8 @@ pkgs.mkShell {
 		golangci-lint
 
 		run
+
+		ctpTW
 	];
 }
 
