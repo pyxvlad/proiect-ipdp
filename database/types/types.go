779592217f32bfe2aad5a7cddf4f22bcdf738b28
vlad <vlad.anghel@protonmail.com>
2024-05-09 19:44:41 +0300

    implement stringer for database/types.Status

--- database/types/types.go
+++ database/types/types.go
@@ -5,18 +5,21 @@ type BookID int64
 type ProgressID int64
 
 const (
-	InvalidBookID BookID = 0;
-	InvalidAccountID AccountID = 0;
-	InvalidProgressID ProgressID = 0;
+	InvalidBookID     BookID     = 0
+	InvalidAccountID  AccountID  = 0
+	InvalidProgressID ProgressID = 0
 )
 
 type Status string
 
 const (
-	StatusToBeRead Status = "to be read"
+	StatusToBeRead   Status = "to be read"
 	StatusInProgress Status = "in progress"
-	StatusRead Status = "read"
-	StatusDropped Status = "dropped"
-	StatusUncertain Status = "uncertain"
+	StatusRead       Status = "read"
+	StatusDropped    Status = "dropped"
+	StatusUncertain  Status = "uncertain"
 )
 
+func (s Status) String() string {
+	return string(s)
+}
